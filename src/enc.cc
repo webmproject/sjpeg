2374ba9f281c71ab481e64490bb3e0137a6e70d4
James Zern <jzern@google.com>
2024-05-18 01:37:09 +0000

    InitializeStaticPointers: make init thread safe
    fixes TSan warnings:
    Read of size 8 at 0x7f6c00f850e8 by thread T914:
        #0 InitializeStaticPointers
        #1 sjpeg::Encoder::Encoder(SjpegYUVMode, int, int, sjpeg::ByteSink*)
    ...
      Previous write of size 8 at 0x7f6c00f850e8 by thread T875:
        #0 InitializeStaticPointers
        #1 sjpeg::Encoder::Encoder(SjpegYUVMode, int, int, sjpeg::ByteSink*)

--- src/enc.cc
+++ src/enc.cc
@@ -20,6 +20,8 @@
 #include <math.h>
 #include <float.h>    // for FLT_MAX
 #include <stdint.h>
+
+#include <mutex>  // NOLINT
 #include <new>
 
 #define SJPEG_NEED_ASM_HEADERS
@@ -256,12 +258,13 @@ void (*Encoder::fDCT_)(int16_t* in, int num_blocks) = nullptr;
 Encoder::StoreHistoFunc Encoder::store_histo_ = nullptr;
 
 void Encoder::InitializeStaticPointers() {
-  if (fDCT_ == nullptr) {
+  static std::once_flag once;
+  std::call_once(once, []() {
     store_histo_ = GetStoreHistoFunc();
     quantize_block_ = GetQuantizeBlockFunc();
     quantize_error_ = GetQuantizeErrorFunc();
     fDCT_ = GetFdct();
-  }
+  });
 }
 
 ////////////////////////////////////////////////////////////////////////////////
